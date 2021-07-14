9f0a524a54268eac11ad0452c8d167aa3f3213f2
sumitgirigit <sgiri5667@gmail.com>
2021-07-14 11:47:22 -0700

    commit

--- Vue/first-app/src/App.vue
+++ Vue/first-app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App Sumit"/>
+    <HelloWorld msg="Welcome to Your Vue.js App Sumit "/>
   </div>
 </template>
 
